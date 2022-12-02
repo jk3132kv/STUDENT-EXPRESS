5eb36a8b0e164afe957f737862a8f2ba14e3bf47
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-12-01 21:57:20 -0600

    Lab 12 changes

--- student-sign-in-client/src/App.vue
+++ student-sign-in-client/src/App.vue
@@ -45,8 +45,7 @@ export default {
         this.updateStudents()
       })
       .catch( err => {
-        let msg = err.response.data.join(',')
-        alert('Error adding student\n' + msg)
+        alert('Error adding student. StarID must be unique.')
       })
     },
     studentArrivedOrLeft(student, present) {
